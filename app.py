608c46d803ddec1722bf4316c5432aeccf7407a0
Kristerpher Henderson <kris@moosequest.net>
2024-06-05 00:53:28 -0700

    refactor: update and ensure logic is working

--- app.py
+++ app.py
@@ -76,33 +76,69 @@ def roll():
         'strikes': game_data['strikes']
     }
 
-    if game_data['come_out_roll'] or len(game_data['established_points']) < 4:
-        if roll_sum in [7, 11]:
-            game_data['bankroll'] -= initial_bet
-            game_data['losses'] += initial_bet
-            response['status'] = 'Seven or Eleven rolled, you lost the Don\'t Pass bet'
-            response['summary_button'] = True
-            response['continue_button'] = True
-            game_data['come_out_roll'] = True  # Reset to come-out roll
-        elif roll_sum in [2, 3]:
-            game_data['bankroll'] += initial_bet
-            game_data['wins'] += initial_bet
-            response['status'] = 'Two or Three rolled, you won the Don\'t Pass bet'
-        elif roll_sum == 12:
-            response['status'] = 'Twelve rolled, it\'s a push'
-        else:
-            game_data['established_points'].append(roll_sum)
-            game_data['come_out_roll'] = False  # Not the come-out roll anymore
-            if roll_sum in [4, 10]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 90 if game_data['initial_bankroll'] == 1000 else 100, 'potential_win': 45 if game_data['initial_bankroll'] == 1000 else 100})
-            elif roll_sum in [5, 9]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 60 if game_data['initial_bankroll'] == 1000 else 75, 'potential_win': 40 if game_data['initial_bankroll'] == 1000 else 75})
-            elif roll_sum in [6, 8]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 30 if game_data['initial_bankroll'] == 1000 else 60, 'potential_win': 25 if game_data['initial_bankroll'] == 1000 else 60})
-            if len(game_data['established_points']) < 4:
-                game_data['current_bets'].append({'type': 'Don\'t Come', 'bet': initial_bet, 'potential_win': initial_bet})
+    if bet_choice == 'bet':
+        if game_data['come_out_roll'] or len(game_data['established_points']) < 4:
+            if roll_sum in [7, 11]:
                 game_data['bankroll'] -= initial_bet
-            response['status'] = 'Point established. Place a $' + str(initial_bet) + ' Don\'t Come bet.'
+                game_data['losses'] += initial_bet
+                response['status'] = 'Seven or Eleven rolled, you lost the Don\'t Pass bet'
+                response['summary_button'] = True
+                response['continue_button'] = True
+                game_data['come_out_roll'] = True  # Reset to come-out roll
+            elif roll_sum in [2, 3]:
+                game_data['bankroll'] += initial_bet
+                game_data['wins'] += initial_bet
+                response['status'] = 'Two or Three rolled, you won the Don\'t Pass bet'
+            elif roll_sum == 12:
+                response['status'] = 'Twelve rolled, it\'s a push'
+            else:
+                game_data['established_points'].append(roll_sum)
+                game_data['come_out_roll'] = False  # Not the come-out roll anymore
+                if roll_sum in [4, 10]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 90 if game_data['initial_bankroll'] == 1000 else 100, 'potential_win': 45 if game_data['initial_bankroll'] == 1000 else 100})
+                elif roll_sum in [5, 9]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 60 if game_data['initial_bankroll'] == 1000 else 75, 'potential_win': 40 if game_data['initial_bankroll'] == 1000 else 75})
+                elif roll_sum in [6, 8]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 30 if game_data['initial_bankroll'] == 1000 else 60, 'potential_win': 25 if game_data['initial_bankroll'] == 1000 else 60})
+                if len(game_data['established_points']) < 4:
+                    game_data['current_bets'].append({'type': 'Don\'t Come', 'bet': initial_bet, 'potential_win': initial_bet})
+                    game_data['bankroll'] -= initial_bet
+                response['status'] = 'Point established. Place a $' + str(initial_bet) + ' Don\'t Come bet.'
+        else:
+            if roll_sum == 7:
+                total_wins = sum(bet['potential_win'] for bet in game_data['current_bets'] if bet.get('point') in game_data['established_points'])
+                game_data['bankroll'] += total_wins
+                game_data['wins'] += total_wins
+                if game_data['come_out_roll']:
+                    response['status'] = 'Seven rolled on come-out roll, you lost the Don\'t Pass bet'
+                    game_data['losses'] += initial_bet
+                    game_data['bankroll'] -= initial_bet
+                else:
+                    response['status'] = 'Seven rolled, you won on all points'
+                response['summary_button'] = True
+                response['continue_button'] = True
+                game_data['come_out_roll'] = True  # Reset to come-out roll
+                game_data['established_points'] = []  # Clear all established points
+            elif roll_sum in game_data['established_points']:
+                point_bet = next((bet for bet in game_data['current_bets'] if bet.get('point') == roll_sum), None)
+                game_data['bankroll'] -= (initial_bet + point_bet['bet'] if point_bet else 0)
+                game_data['losses'] += (initial_bet + point_bet['bet'] if point_bet else 0)
+                game_data['points_lost'] += 1
+                game_data['established_points'].remove(roll_sum)
+                response['status'] = 'Point hit. Don\'t Behind ' + str(roll_sum) + ' and it\'s replaced by your Don\'t Come bet. This is strike ' + str(game_data['strikes'] + 1) + '. Place another Don\'t Come bet.'
+                game_data['strikes'] += 1
+                if game_data['strikes'] >= 3:
+                    response['status'] += ' Three strikes, wait for a 7 before betting again.'
+                    game_data['current_bets'] = []
+            else:
+                if roll_sum in [4, 10]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 90 if game_data['initial_bankroll'] == 1000 else 100, 'potential_win': 45 if game_data['initial_bankroll'] == 1000 else 100})
+                elif roll_sum in [5, 9]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 60 if game_data['initial_bankroll'] == 1000 else 75, 'potential_win': 40 if game_data['initial_bankroll'] == 1000 else 75})
+                elif roll_sum in [6, 8]:
+                    game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 30 if game_data['initial_bankroll'] == 1000 else 60, 'potential_win': 25 if game_data['initial_bankroll'] == 1000 else 60})
+                game_data['established_points'].append(roll_sum)
+                response['status'] = 'Point established. Place a $' + str(initial_bet) + ' Don\'t Come bet.'
     else:
         if roll_sum == 7:
             total_wins = sum(bet['potential_win'] for bet in game_data['current_bets'] if bet.get('point') in game_data['established_points'])
@@ -117,6 +153,7 @@ def roll():
             response['summary_button'] = True
             response['continue_button'] = True
             game_data['come_out_roll'] = True  # Reset to come-out roll
+            game_data['established_points'] = []  # Clear all established points
         elif roll_sum in game_data['established_points']:
             point_bet = next((bet for bet in game_data['current_bets'] if bet.get('point') == roll_sum), None)
             game_data['bankroll'] -= (initial_bet + point_bet['bet'] if point_bet else 0)
@@ -134,87 +171,6 @@ def roll():
     game_data['results'].append(dice_roll)
     game_data['roll_count'] += 1
     db.games.update_one({'_id': game_id}, {'$set': game_data})
-
-    response['bankroll'] = game_data['bankroll']
-    response['wins'] = game_data['wins']
-    response['losses'] = game_data['losses']
-    response['established_points'] = game_data['established_points']
-    response['strikes'] = game_data['strikes']
-
-    return jsonify(response)
-
-@app.route('/take_action', methods=['POST'])
-def take_action():
-    action = request.form['action']
-    game_id = ObjectId(session['game_id'])
-    game_data = db.games.find_one({'_id': game_id})
-    
-    response = {
-        'status': 'continue',
-        'current_bets': game_data['current_bets'],
-        'bankroll': game_data['bankroll'],
-        'results': game_data['results'],
-        'roll_count': game_data['roll_count'],
-        'wins': game_data['wins'],
-        'losses': game_data['losses'],
-        'established_points': game_data['established_points'],
-        'strikes': game_data['strikes']
-    }
-
-    if action == 'continue':
-        dice_roll = roll_dice()
-        roll_sum = sum(dice_roll)
-        response['roll_sum'] = roll_sum
-        response['dice_roll'] = dice_roll
-        initial_bet = 25 if game_data['initial_bankroll'] == 1000 else 50
-
-        if roll_sum == 7:
-            total_wins = sum(bet['potential_win'] for bet in game_data['current_bets'] if bet.get('point') in game_data['established_points'])
-            game_data['bankroll'] += total_wins
-            game_data['wins'] += total_wins
-            if game_data['come_out_roll']:
-                response['status'] = 'Seven rolled on come-out roll, you lost the Don\'t Pass bet'
-                game_data['losses'] += initial_bet
-                game_data['bankroll'] -= initial_bet
-            else:
-                response['status'] = 'Seven rolled, you won on all points'
-            response['summary_button'] = True
-            response['continue_button'] = True
-            game_data['come_out_roll'] = True  # Reset to come-out roll
-        elif roll_sum in game_data['established_points']:
-            point_bet = next((bet for bet in game_data['current_bets'] if bet.get('point') == roll_sum), None)
-            game_data['bankroll'] -= (initial_bet + point_bet['bet'] if point_bet else 0)
-            game_data['losses'] += (initial_bet + point_bet['bet'] if point_bet else 0)
-            game_data['points_lost'] += 1
-            game_data['established_points'].remove(roll_sum)
-            response['status'] = 'Point hit. Don\'t Behind ' + str(roll_sum) + ' and it\'s replaced by your Don\'t Come bet. This is strike ' + str(game_data['strikes'] + 1) + '. Place another Don\'t Come bet.'
-            game_data['strikes'] += 1
-            if game_data['strikes'] >= 3:
-                response['status'] += ' Three strikes, wait for a 7 before betting again.'
-                game_data['current_bets'] = []
-        else:
-            if roll_sum in [4, 10]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 90 if game_data['initial_bankroll'] == 1000 else 100, 'potential_win': 45 if game_data['initial_bankroll'] == 1000 else 100})
-            elif roll_sum in [5, 9]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 60 if game_data['initial_bankroll'] == 1000 else 75, 'potential_win': 40 if game_data['initial_bankroll'] == 1000 else 75})
-            elif roll_sum in [6, 8]:
-                game_data['current_bets'].append({'type': 'Lay Odds', 'point': roll_sum, 'bet': 30 if game_data['initial_bankroll'] == 1000 else 60, 'potential_win': 25 if game_data['initial_bankroll'] == 1000 else 60})
-            game_data['established_points'].append(roll_sum)
-            response['status'] = 'Point established. Place a $' + str(initial_bet) + ' Don\'t Come bet.'
-
-        game_data['results'].append(dice_roll)
-        game_data['roll_count'] += 1
-        db.games.update_one({'_id': game_id}, {'$set': game_data})
-
-    elif action == 'no_bet':
-        dice_roll = roll_dice()
-        roll_sum = sum(dice_roll)
-        response['roll_sum'] = roll_sum
-        response['dice_roll'] = dice_roll
-        response['status'] = 'No bet'
-        game_data['results'].append(dice_roll)
-        game_data['roll_count'] += 1
-        db.games.update_one({'_id': game_id}, {'$set': game_data})
 
     response['bankroll'] = game_data['bankroll']
     response['wins'] = game_data['wins']
