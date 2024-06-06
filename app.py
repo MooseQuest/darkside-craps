cd05eaaf5964a572189b3c5893df4160d1ef5724
Kristerpher Henderson <kris@moosequest.net>
2024-06-06 13:12:32 -0700

    ci: heroku configurations

--- app.py
+++ app.py
@@ -274,4 +274,5 @@ def summary():
     return jsonify(summary)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
